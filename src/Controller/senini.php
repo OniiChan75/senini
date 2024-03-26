0abd46a429131cda8b9fad44545eef23c57902b5
OniiChan75 <melesgabriele@gmail.com>
2024-03-26 18:26:47 +0100

    creat index and add twig templates

---
+++ src/Controller/senini.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class senini extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function homepage() : Response
+    {
+        return $this->render('senini/homepage.html.twig', [
+            'title' => 'senini fabric',
+        ]);
+    }
+
+}
